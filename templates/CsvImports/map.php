464fcd7b2a9ab60a2a9238f95d7a172f11723dd1
Jason Tempestini <jason@curlymedia.com>
2021-07-07 12:49:56 -0700

    next steps

--- templates/CsvImports/map.php
+++ templates/CsvImports/map.php
@@ -8,9 +8,7 @@ use App\View\AppView;
  * @var array $source_columns
  * @var string $target_table
  */
-
-debug($target_table);
-
+echo $this->Html->tag('h2', "Importing to $target_table");
 echo $this->Form->create();
 foreach ($source_columns as $source_column) {
     echo $this->Form->control($source_column, ['type' => 'select', 'options' => $target_columns, 'empty' => 'choose a target column']);
